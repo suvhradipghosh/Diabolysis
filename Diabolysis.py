e54814b104d4fc4dc65e26bc892aa402dfbd9a12
Suvhradip ghosh <42653351+suvhradipghosh@users.noreply.github.com>
2018-10-09 20:54:14 +0530

    Add files via upload

---
+++ Diabolysis.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('diabetes.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 8].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+'''from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()'''
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y, test_size=0.2 ,random_state=0)
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train , y_train)
+
+'''#prediction
+y_pred=regressor.predict(X_train)'''
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_train)
